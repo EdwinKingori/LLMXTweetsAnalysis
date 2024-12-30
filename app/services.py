bdc08d22dc2edbd5dafd50b93fc31eb47bca76e4
Edwin King'ori <eddmunyiri@gmail.com>
2024-12-30 16:46:49 +0300

    Debugging and handling instructor.exceptions errors

--- app/services.py
+++ app/services.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 from .database import SessionLocal
 from .models import Tweet
 from .schemas import TweetAnalysis
+from instructor.exceptions import InstructorRetryException
 
 # initializing the client
 client = instructor.from_openai(OpenAI())
@@ -21,17 +22,30 @@ def analyze_tweets():
 #  In this case, the model used is "gpt-4o-mini" using a response prompt
 
     for tweet in tweets:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{
-                "role": "user",
-                "content": f"Classify this tweet as political, offensive or neutral only if confident: '{tweet.text}'"
-            }],
-        )
+        try:
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[{
+                    "role": "user",
+                    "content": f"Classify this tweet as political, offensive or neutral only if confident: '{tweet.x_tweets}'"
+                }],
+                response_model=TweetAnalysis
+            )
+
+        except InstructorRetryException as e:
+            error_message = str(e)
+            if 'exceed_quota' in error_message:
+                print(
+                    "Error: You exceeded your quota. Please check your billing details.")
+            else:
+                print(f"An error occured: {error_message}")
+            break
+        except Exception as e:
+            print(f"An error occured: {str(e)}")
 
         analysis_result = TweetAnalysis(
             id=tweet.id,
-            text=tweet.text,
+            text=tweet.x_tweets,
             isPolitical=response.get("isPolitical", False),
             isOffensive=response.get("isOffensive", False),
             isNeutral=response.get("isNeutral", False),
